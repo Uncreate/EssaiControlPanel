0589972d986651a507e3372f3a7e4b16327eb386
Uncreate <ajriggs829@gmail.com>
2023-10-26 16:41:25 -0500

    Added PG002048A rough/finish

--- excel/cleaner.bat
+++
@@ -1,8 +0,0 @@
-echo off
-robocopy c:\EssaiControlPanel\excel c:\EssaiControlPanel\excel\AL AL_**.xls /log+:robolog.log
-robocopy c:\EssaiControlPanel\excel c:\EssaiControlPanel\excel\CU CU_**.xls /log+:robolog.log
-robocopy c:\EssaiControlPanel\excel c:\EssaiControlPanel\excel\PG PG_**.xls /log+:robolog.log
-robocopy c:\EssaiControlPanel\excel c:\EssaiControlPanel\excel\PL PL_**.xls /log+:robolog.log
-robocopy c:\EssaiControlPanel\excel c:\EssaiControlPanel\excel\S1 S1_**.xls /log+:robolog.log
-Pause
-del S1_**.xls PL_**.xls PG_**.xls CU_**.xls AL_**.xls
